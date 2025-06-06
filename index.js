bb5952abeb96d10a42bfca91751a85b8d0cef928
林屁屁愛噗噗 <aptflin@linpipiaipupudeMacBook-Air.local>
2025-06-06 19:14:09 +0800

    fix index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
-
-
-
 const http = require('http');
 const WebSocket = require('ws');
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 // ✅ 讓 Express 公開整個 src 資料夾給瀏覽器使用
 app.use('/src', express.static(path.join(__dirname, 'src')));
@@ -17,22 +13,6 @@ app.use('/src', express.static(path.join(__dirname, 'src')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-
-
-/*const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const path = require('path');
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname)));
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -52,4 +32,4 @@ wss.on('connection', ws => {
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-});*/
+});
