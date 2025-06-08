94e11c404c9df306d69b9d579d13f294f511865a
林屁屁愛噗噗 <aptflin@linpipiaipupudeMacBook-Air.local>
2025-06-09 01:51:12 +0800

    add TD can send to web

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log('我有進來script.js')
 let ws = new WebSocket('wss://websitetest-xx30.onrender.com');
 
+
+let controledByTD = document.querySelector('.controledByTD');
+
+
+//林屁屁會用網頁測控TD了ㄎㄎ 
 let controlTD = document.querySelector('.controlTD');
 controlTD.addEventListener('input',(event) =>{
     ws.send(JSON.stringify({'slider1': controlTD.value}))
@@ -10,8 +15,19 @@ ws.addEventListener('open',(event) =>{
     console.log('websocket opend')
 });
 
-ws.addEventListener('message',(massage) =>{
-    console.log(message);
+ws.addEventListener('message',(message) =>{
+    if (message.data == 'ping'){
+        ws.send('pong');
+        return
+    }
+
+    let data = JSON.parse(message.data);
+    if('slider1' in data){
+        let val = data['slider1'];
+        controledByTD.value = val*100;
+        
+    }
+    console.log(data);
 });
 
 ws.addEventListener('error',(error) =>{
